8f559979d2291fc49e3daa2183a1c9b48364ef82
Hiren Khatri <khatrih879@gmail.com>
2026-02-27 13:26:32 +0530

    Scaffolded vite@react codebase and created basic terminal ui

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='root'>
+      <div className='flex justify-center items-center bg-[#1a1a1a] text-[#888] title-bar'>
+        <p>visitor@portfolio</p>
+      </div>
+    </div>
+
+  )
+}
+
+export default App
